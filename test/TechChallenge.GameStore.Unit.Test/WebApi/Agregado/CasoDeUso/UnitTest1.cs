ad86b8cbc18ff8575c7eb3498c971693ec0aca07
Bárbara Antunes Pacheco <1518575@sga.pucminas.br>
2025-07-12 13:08:06 -0300

    feat: cria estrutura do projeto

---
+++ test/TechChallenge.GameStore.Unit.Test/WebApi/Agregado/CasoDeUso/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace TechChallenge.GameStore.Unit.Test.WebApi.Agregado.CasoDeUso;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
