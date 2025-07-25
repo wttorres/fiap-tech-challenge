ce0b0d3dce277af0fc7f04d4ea5da87b7b7a4154
Winicius Torres <rm366458@fiap.com.br>
2025-07-25 13:18:09 -0300

    #9 - Correção de Migrations

--- src/TechChallenge.GameStore.Infrastructure/Migrations/20250725160104_MigracaoInicial.cs
+++ src/TechChallenge.GameStore.Infrastructure/Migrations/20250725160104_MigracaoInicial.cs
@@ -7,7 +7,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace TechChallenge.GameStore.Infrastructure.Migrations
 {
     /// <inheritdoc />
-    public partial class AddPromocao : Migration
+    public partial class MigracaoInicial : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
@@ -42,6 +42,23 @@ namespace TechChallenge.GameStore.Infrastructure.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    Senha = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    CriadoEm = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    Perfil = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuario", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "promocao_jogo",
                 columns: table => new
                 {
@@ -52,22 +69,42 @@ namespace TechChallenge.GameStore.Infrastructure.Migrations
                 {
                     table.PrimaryKey("PK_promocao_jogo", x => new { x.PromocaoId, x.JogoId });
                     table.ForeignKey(
+                        name: "FK_promocao_jogo_jogo_JogoId",
+                        column: x => x.JogoId,
+                        principalTable: "jogo",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
                         name: "FK_promocao_jogo_promocao_PromocaoId",
                         column: x => x.PromocaoId,
                         principalTable: "promocao",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
                 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_promocao_jogo_JogoId",
+                table: "promocao_jogo",
+                column: "JogoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_usuario_Email",
+                table: "usuario",
+                column: "Email",
+                unique: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropTable(
-                name: "jogo");
+                name: "promocao_jogo");
 
             migrationBuilder.DropTable(
-                name: "promocao_jogo");
+                name: "usuario");
+
+            migrationBuilder.DropTable(
+                name: "jogo");
 
             migrationBuilder.DropTable(
                 name: "promocao");
