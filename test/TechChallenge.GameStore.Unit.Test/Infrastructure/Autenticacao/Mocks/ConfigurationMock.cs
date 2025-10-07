af0697c77b56af9759e916f6d610984b9ccb0658
Bárbara Antunes Pacheco <1518575@sga.pucminas.br>
2025-10-06 22:49:10 -0300

    bugfix: ajuste teste e inclui dependencia do CI para deploy

--- test/TechChallenge.GameStore.Unit.Test/Infrastructure/Autenticacao/Mocks/ConfigurationMock.cs
+++ test/TechChallenge.GameStore.Unit.Test/Infrastructure/Autenticacao/Mocks/ConfigurationMock.cs
@@ -7,10 +7,8 @@ public class ConfigurationMock : Mock<IConfiguration>
 {
     public void ConfigurarValoresPadroesJwt()
     {
-        Setup(c => c["Jwt:Key"]).Returns("chave-secreta-super-segura-para-testes");
-        Setup(c => c["Jwt:Issuer"]).Returns("GameStore.Issuer.Teste");
-        Setup(c => c["Jwt:Audience"]).Returns("GameStore.Audience.Teste");
+        Setup(c => c["JWT_KEY"]).Returns("chave-secreta-super-segura-para-testes");
+        Setup(c => c["JWT_ISSUER"]).Returns("GameStore.Issuer.Teste");
+        Setup(c => c["JWT_AUDIENCE"]).Returns("GameStore.Audience.Teste");
     }
-
-    public string ObterChave() => "chave-secreta-super-segura-para-testes";
 }
